0a8cfad44c3eaa3e53b87a8c1f2eaf18748b33fc
Staykova_Alona <380666091645Abyss@gmail.com>
2026-08-28 20:41:07 +0300

    refactor: rename Application and ApplicationTests classes with package

---
+++ src/main/java/com/github/plantfern/foodDiary/FoodDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.github.plantfern.foodDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDiaryApplication.class, args);
+    }
+
+}
