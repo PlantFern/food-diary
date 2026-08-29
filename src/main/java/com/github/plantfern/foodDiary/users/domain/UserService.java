c13dd860d3184409484be4e15d56c9557b3709a2
Staykova_Alona <380666091645Abyss@gmail.com>
2026-08-29 19:53:39 +0300

    feat(users): implement UserApi in UserService

---
+++ src/main/java/com/github/plantfern/foodDiary/users/domain/UserService.java
@@ -0,0 +1,97 @@
+package com.github.plantfern.foodDiary.users.domain;
+
+import com.github.plantfern.foodDiary.users.api.RoleName;
+import com.github.plantfern.foodDiary.users.api.UserApi;
+import com.github.plantfern.foodDiary.users.api.UserDto;
+import com.github.plantfern.foodDiary.users.domain.entities.RoleEntity;
+import com.github.plantfern.foodDiary.users.domain.entities.UserEntity;
+import com.github.plantfern.foodDiary.users.domain.repositories.RoleRepository;
+import com.github.plantfern.foodDiary.users.domain.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserService implements UserApi {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserService(
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder,
+            UserMapper userMapper){
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
+    } // UserService
+
+    public void save(UserEntity user){
+        userRepository.save(user);
+    }
+
+
+    // реализация интерфейса
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<UserDto> findById(Long id) {
+        return userRepository
+                .findById(id)
+                .map(userMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<UserDto> findByEmail(String email) {
+        return userRepository
+                .findByEmailIgnoreCase(email)
+                .map(userMapper::toDto);
+    }
+
+    @Override
+    public UserDto register(String email, String password) {
+        if(userRepository.existsByEmailAndDeletedAt(email)){
+            throw new IllegalArgumentException("Email alreade registered");
+        }
+
+        UserEntity user = new UserEntity(
+                email,
+                passwordEncoder.encode(password)
+        );
+
+        RoleEntity defaultRole = roleRepository
+                .findByName(RoleName.USER)
+                .orElseThrow(() -> new IllegalStateException("Role user is missing"));
+        user.addRole(defaultRole);
+
+        UserEntity saved = userRepository.save(user);
+        return userMapper.toDto(saved);
+    }
+
+    @Override
+    public void assignRoles(Long userId, Set<RoleName> roles) {
+        UserEntity user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Set<RoleEntity> newRoles = roleRepository.findByNameIn(roles);
+        user.replaceRoles(newRoles);
+    }
+
+    @Override
+    public boolean hasRole(Long userId, RoleName role) {
+        return userRepository.findById(userId)
+                .map(user -> user.getUserRoles().stream()
+                        .anyMatch(ur -> ur.getRole().getName().equals(role)))
+                .orElse(false);
+    }
+} // UserService
