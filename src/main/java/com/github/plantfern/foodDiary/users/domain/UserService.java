36cb573e91dadb7867d671a30e794ab9783860ac
Staykova_Alona <380666091645Abyss@gmail.com>
2026-08-31 00:37:41 +0300

    feat(users): add methods to find active users (where deletedAt is null)

--- src/main/java/com/github/plantfern/foodDiary/users/domain/UserService.java
+++ src/main/java/com/github/plantfern/foodDiary/users/domain/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -37,6 +38,12 @@ public class UserService implements UserApi {
 
     public void save(UserEntity user){
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserEntity> getAll() {
+        return userRepository
+                .findAllByDeletedAtIsNull();
     }
 
 
