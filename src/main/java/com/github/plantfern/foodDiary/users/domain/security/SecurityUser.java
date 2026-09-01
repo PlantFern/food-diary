4d74d6912ba8b988fe6a2a368789494300eb6657
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-01 13:56:13 +0300

    feat(security): create SecurityUser implementing UserDetails and Serializable

---
+++ src/main/java/com/github/plantfern/foodDiary/users/domain/security/SecurityUser.java
@@ -0,0 +1,48 @@
+package com.github.plantfern.foodDiary.users.domain.security;
+
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Set;
+
+
+public class SecurityUser implements UserDetails, Serializable {
+
+    @Getter
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final Set<GrantedAuthority> authorities;
+
+    public SecurityUser(
+            Long id,
+            String email,
+            String password,
+            Collection<? extends GrantedAuthority> authorities
+    ) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = Set.copyOf(authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
