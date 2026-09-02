b0c2f21a58ae131d302bfe5abb92ffb6896bf69c
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-02 23:43:49 +0300

    feat(specialists): add UserRelationStatusEntity and UserRelationStatus enumerator

---
+++ src/main/java/com/github/plantfern/foodDiary/specialists/domain/UserRelationStatus.java
@@ -0,0 +1,8 @@
+package com.github.plantfern.foodDiary.specialists.domain;
+
+public enum UserRelationStatus {
+    PENDING,
+    ACTIVE,
+    REJECTED,
+    ENDED
+}
---
+++ src/main/java/com/github/plantfern/foodDiary/specialists/domain/entities/UserRelationStatusEntity.java
@@ -0,0 +1,34 @@
+package com.github.plantfern.foodDiary.specialists.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+@Entity
+@Table(
+        name="user_relation_statuses",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "unique_user_relation_statuses_code", columnNames = "code")
+        }
+)
+public class UserRelationStatusEntity {
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length=20)
+    private String code;
+
+
+    protected UserRelationStatusEntity(){}
+
+    private UserRelationStatusEntity(String code){
+        this.code = code;
+    }
+}
