462670e3152dc042d351dfa98c76f9716bf6bb9f
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-02 15:25:13 +0300

    feat(diary_profiles): Add gender entity

---
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/entities/GenderEntity.java
@@ -0,0 +1,42 @@
+package com.github.plantfern.foodDiary.diaryProfiles.domain.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+
+@Entity
+@Table(
+        name = "genders",
+        uniqueConstraints = @UniqueConstraint(columnNames = {
+                "code"
+        })
+)
+public class GenderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30)
+    private String code;
+
+    @OneToMany(
+            mappedBy = "gender",
+            fetch = FetchType.LAZY
+    )
+    private Set<DiaryProfileEntity> diaryProfileGenders = new HashSet<>();
+
+
+    protected GenderEntity() {}
+    private GenderEntity(String code){
+        this.code = code;
+    }
+}
