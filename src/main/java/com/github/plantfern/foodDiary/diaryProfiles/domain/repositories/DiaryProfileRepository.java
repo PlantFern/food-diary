c59b8c8dc70cea44b21cad73c7981cec89262f36
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-05 10:31:31 +0300

    refactor(diaryProfiles): add @Query annotation to findUserIdById method

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/repositories/DiaryProfileRepository.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/repositories/DiaryProfileRepository.java
@@ -2,6 +2,8 @@ package com.github.plantfern.foodDiary.diaryProfiles.domain.repositories;
 
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.DiaryProfileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,11 @@ import java.util.Optional;
 @Repository
 public interface DiaryProfileRepository extends JpaRepository<DiaryProfileEntity, Long> {
     boolean existsByUserId(Long userId);
-    Long findUserIdById(Long id);
+
+    @Query("""
+        select dp.userId
+        from DiaryProfileEntity dp
+        where dp.id = :id
+        """)
+    Long findUserIdById(@Param("id") Long id);
 }
