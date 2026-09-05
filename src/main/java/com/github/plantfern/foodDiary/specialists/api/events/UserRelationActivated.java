d08a16e13732e8e68c59880cc17b11bb9aa01b6c
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-05 11:37:30 +0300

    fix(users): remove unused argument from UserRelationActivated

--- src/main/java/com/github/plantfern/foodDiary/specialists/api/events/UserRelationActivated.java
+++ src/main/java/com/github/plantfern/foodDiary/specialists/api/events/UserRelationActivated.java
@@ -3,7 +3,6 @@ package com.github.plantfern.foodDiary.specialists.api.events;
 import com.github.plantfern.foodDiary.specialists.api.RelationType;
 
 public record UserRelationActivated(
-        Long relationId,
         Long diaryProfileUserId,
         Long specialistUserId,
         String relationType
