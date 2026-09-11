1c03570ab0da37288c9233715945fd96996a33a4
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-11 19:32:38 +0300

    fix(diaryProfiles): add componentModel = "spring" to @Mapper annotation

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/ProfileFeatureSettingsMapper.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/ProfileFeatureSettingsMapper.java
@@ -7,7 +7,8 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper
+
+@Mapper(componentModel = "spring")
 public interface ProfileFeatureSettingsMapper {
 
     ProfileFeatureSettingsDto toDto(ProfileFeatureSettingsEntity profileFeatureSettings);
