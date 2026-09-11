301df3a1adaf06fc302be948ef3994f7190956db
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-11 21:48:07 +0300

    fix(diaryProfiles): query diaryProfileId by createdBy without loading full entity

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/repositories/ProfileFeatureSettingsRepository.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/repositories/ProfileFeatureSettingsRepository.java
@@ -3,6 +3,7 @@ package com.github.plantfern.foodDiary.diaryProfiles.domain.repositories;
 
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.ProfileFeatureSettingsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,11 @@ public interface ProfileFeatureSettingsRepository extends JpaRepository<ProfileF
 
     List<ProfileFeatureSettingsEntity> findAllByDiaryProfileId(Long diaryProfileId);
 
+    @Query("""
+        select profile_feature_settings.diaryProfileId as DiaryProfileId
+        from ProfileFeatureSettingsEntity profile_feature_settings
+        where profile_feature_settings.createdById = :userId
+    """)
     List<Long> findAllDiaryProfileIdByCreatedById(Long userId);
 
     List<ProfileFeatureSettingsEntity> findAllByCreatedById(Long userId);
