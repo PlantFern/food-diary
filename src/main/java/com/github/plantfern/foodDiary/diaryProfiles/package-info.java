005b66034dc4b1c089c589840014dee6eae808a4
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-12 13:03:14 +0300

    feat(diaryProfiles): add package-info.java for diaryProfile module

---
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/package-info.java
@@ -0,0 +1,7 @@
+
+@ApplicationModule(
+        allowedDependencies = {"common", "users :: api"}
+)
+package com.github.plantfern.foodDiary.diaryProfiles;
+
+import org.springframework.modulith.ApplicationModule;
