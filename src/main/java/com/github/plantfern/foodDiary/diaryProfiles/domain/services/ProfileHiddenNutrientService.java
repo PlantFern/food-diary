d13c824f887d871b615e3f5ae23ccb46cfde5c53
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-13 00:16:12 +0300

    feat(diaryProfiles): add addToSettings and addAllToSettings to ProfileHiddenNutrientService

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/services/ProfileHiddenNutrientService.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/services/ProfileHiddenNutrientService.java
@@ -1,12 +1,16 @@
 package com.github.plantfern.foodDiary.diaryProfiles.domain.services;
 
 
+import com.github.plantfern.foodDiary.diaryProfiles.domain.ProfileFeatureSettingsMapper;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.ProfileHiddenNutrientEntity;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.repositories.ProfileHiddenNutrientRepository;
+import com.github.plantfern.foodDiary.diaryProfiles.domain.security.DiaryProfilePolicy;
+import com.github.plantfern.foodDiary.users.api.CurrentUser;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,7 +21,51 @@ public class ProfileHiddenNutrientService {
     private final ProfileHiddenNutrientRepository profileHiddenNutrientRepository;
     private final DiaryProfileService diaryProfileService;
     private final ProfileFeatureSettingsService profileFeatureSettingsService;
+    private final DiaryProfilePolicy diaryProfilePolicy;
+    private final CurrentUser currentUser;
+    private final ProfileFeatureSettingsMapper profileFeatureSettingsMapper;
 
+
+    public void addToSettings(Long profileFeatureSettingsId, Long nutrientId) {
+
+        var profileFeatureSettings = profileFeatureSettingsService
+                .getByIdInternal(profileFeatureSettingsId);
+
+        diaryProfilePolicy.ensureIsOwner(currentUser, profileFeatureSettings.createdById());
+
+        profileHiddenNutrientRepository.save(
+                new ProfileHiddenNutrientEntity(
+                        profileFeatureSettingsMapper
+                                .toEntity(profileFeatureSettings),
+                        nutrientId
+                )
+        );
+    }
+
+    public void addAllToSettings(Long profileFeatureSettingsId, List<Long> nutrientIds) {
+
+        var profileFeatureSettings = profileFeatureSettingsService
+                .getByIdInternal(profileFeatureSettingsId);
+        var profileFeatureSettingsEntity = profileFeatureSettingsMapper
+                .toEntity(profileFeatureSettings);
+
+        diaryProfilePolicy.ensureIsOwner(currentUser, profileFeatureSettings.createdById());
+
+        List<ProfileHiddenNutrientEntity> hiddenNutrientList =
+            nutrientIds
+                    .stream()
+                    .map(nutrientId ->
+                            new ProfileHiddenNutrientEntity(
+                                    profileFeatureSettingsEntity,
+                                    nutrientId
+                            )
+                    )
+                    .toList();
+
+        profileHiddenNutrientRepository.saveAll(
+                hiddenNutrientList
+        );
+    }
 
     public List<ProfileHiddenNutrientEntity> getAllByProfileFeatureSettingsId(
             Long profileFeatureSettingsId
