afae44cf98b1a3511e959120004224b0f2212655
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-12 15:14:05 +0300

    refactor(diaryProfile): add import services.DiaryProfileService in DiaryProfileController

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/DiaryProfileController.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/DiaryProfileController.java
@@ -2,7 +2,7 @@ package com.github.plantfern.foodDiary.diaryProfiles.web;
 
 
 import com.github.plantfern.foodDiary.diaryProfiles.domain.DiaryProfileMapper;
-import com.github.plantfern.foodDiary.diaryProfiles.domain.DiaryProfileService;
+import com.github.plantfern.foodDiary.diaryProfiles.domain.services.DiaryProfileService;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.dto.DiaryProfileDto;
 import com.github.plantfern.foodDiary.diaryProfiles.web.requests.ProfileDataRequest;
 import org.springframework.beans.factory.annotation.Autowired;
