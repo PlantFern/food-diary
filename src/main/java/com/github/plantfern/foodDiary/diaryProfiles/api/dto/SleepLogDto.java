04edcb0233f7fd015e4cc288336e638d1eb1e59f
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-18 14:06:47 +0300

    fix(diaryProfiles): rename parameter to diaryProfileId for request binding

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/api/dto/SleepLogDto.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/api/dto/SleepLogDto.java
@@ -5,7 +5,7 @@ import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.DiaryProfile
 import java.time.LocalDateTime;
 
 public record SleepLogDto(
-        Long diaryProfile,
+        Long diaryProfileId,
         LocalDateTime beganAt,
         LocalDateTime endedAt
 ) { }
--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/api/dto/WeightLogDto.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/api/dto/WeightLogDto.java
@@ -4,6 +4,6 @@ package com.github.plantfern.foodDiary.diaryProfiles.api.dto;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.DiaryProfileEntity;
 
 public record WeightLogDto(
-        Long diaryProfile,
+        Long diaryProfileId,
         Float weight
 ) { }
