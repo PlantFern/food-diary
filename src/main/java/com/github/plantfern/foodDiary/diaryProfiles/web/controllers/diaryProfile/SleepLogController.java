a1a6c0dca2856d4f424f3f788c1490b362036f50
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-19 19:01:20 +0300

    feat(diaryProfiles): add PUT, POST, DELETE endpoints to create, update and delete sleep log

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/diaryProfile/SleepLogController.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/diaryProfile/SleepLogController.java
@@ -1,8 +1,6 @@
 package com.github.plantfern.foodDiary.diaryProfiles.web.controllers.diaryProfile;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.github.plantfern.foodDiary.diaryProfiles.api.dto.SleepLogDto;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.services.SleepLogService;
 import com.github.plantfern.foodDiary.diaryProfiles.web.requests.DatePeriodRequest;
@@ -25,6 +23,47 @@ public class SleepLogController {
     public SleepLogController(SleepLogService sleepLogService) {
         this.sleepLogService = sleepLogService;
     }
+
+    @PutMapping("/{diaryProfileId}")
+    public ResponseEntity<SleepLogDto> create(
+            @PathVariable Long diaryProfileId,
+            @ModelAttribute DateTimePeriodRequest request
+    ) {
+
+        return ResponseEntity.ok(
+                sleepLogService.create(
+                        diaryProfileId,
+                        request.startPeriod(),
+                        request.endPeriod()
+                )
+        );
+    }
+
+    @PutMapping("/{sleepLogId}")
+    public ResponseEntity<SleepLogDto> update(
+            @PathVariable Long sleepLogId,
+            @ModelAttribute DateTimePeriodRequest request
+    ) {
+
+        return ResponseEntity.ok(
+                sleepLogService.update(
+                    sleepLogId,
+                    request.startPeriod(),
+                    request.endPeriod()
+                )
+        );
+    }
+
+    @DeleteMapping("/{sleepLogId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable Long sleepLogId
+    ) {
+
+        sleepLogService
+                .delete(sleepLogId);
+    }
+
 
     @GetMapping("/{sleepLogId}")
     public ResponseEntity<SleepLogDto> getById(
