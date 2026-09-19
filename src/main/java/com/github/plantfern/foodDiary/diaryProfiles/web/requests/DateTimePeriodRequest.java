c109e94484bdf85db5a39d0a48438f964d969c77
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-20 00:56:08 +0300

    fix(diaryProfiles): parse ISO datetime query params

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/requests/DateTimePeriodRequest.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/requests/DateTimePeriodRequest.java
@@ -1,9 +1,11 @@
 package com.github.plantfern.foodDiary.diaryProfiles.web.requests;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 
 public record DateTimePeriodRequest(
-        LocalDateTime startPeriod,
-        LocalDateTime endPeriod
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startPeriod,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endPeriod
 ) {
 }
