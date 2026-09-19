99ccb024f4a17b935894256dea38d0153336c846
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-19 14:35:36 +0300

    feat(diaryProfiles/web): add CUD endpoints for goals

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/diaryProfile/GoalController.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/diaryProfile/GoalController.java
@@ -2,10 +2,14 @@ package com.github.plantfern.foodDiary.diaryProfiles.web.controllers.diaryProfil
 
 
 import com.github.plantfern.foodDiary.diaryProfiles.api.dto.GoalDto;
+import com.github.plantfern.foodDiary.diaryProfiles.api.dto.GoalNutrientDto;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.services.GoalService;
+import com.github.plantfern.foodDiary.diaryProfiles.web.requests.GoalRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,6 +24,65 @@ public class GoalController {
         this.goalService = goalService;
     }
 
+
+    @PutMapping ("/{diaryProfileId}")
+    public ResponseEntity<GoalDto> create(
+            @PathVariable Long diaryProfileId,
+            @RequestBody GoalRequest request
+    ) {
+
+        List<GoalNutrientDto> goalNutrientDtoList = new ArrayList<>();
+        for(var goalNutrient : request.nutrientGoals()){
+            goalNutrientDtoList.add(new GoalNutrientDto(
+                    0L,
+                    goalNutrient.nutrientId(),
+                    goalNutrient.amount()
+            ));
+        }
+
+        return ResponseEntity.ok(goalService.create(
+                diaryProfileId,
+                request.plannedWeight(),
+                request.startDate(),
+                request.plannedEndDate(),
+                goalNutrientDtoList
+                )
+        );
+    }
+
+    @PostMapping ("/{diaryProfileId}")
+    public ResponseEntity<GoalDto> update(
+            @PathVariable Long diaryProfileId,
+            @RequestBody GoalRequest request
+    ) {
+
+        List<GoalNutrientDto> goalNutrientDtoList = new ArrayList<>();
+        for(var goalNutrient : request.nutrientGoals()){
+            goalNutrientDtoList.add(new GoalNutrientDto(
+                    0L,
+                    goalNutrient.nutrientId(),
+                    goalNutrient.amount()
+            ));
+        }
+
+        return ResponseEntity.ok(goalService.create(
+                        diaryProfileId,
+                        request.plannedWeight(),
+                        request.startDate(),
+                        request.plannedEndDate(),
+                        goalNutrientDtoList
+                )
+        );
+    }
+
+    @DeleteMapping("/{diaryProfileId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable Long diaryProfileId
+    ) {
+
+        goalService.delete(diaryProfileId);
+    }
 
     @GetMapping("/{diaryProfileId}")
     public ResponseEntity<List<GoalDto>> getAll(
