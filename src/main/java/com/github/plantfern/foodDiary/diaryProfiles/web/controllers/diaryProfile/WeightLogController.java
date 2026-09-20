e7a45f2465ec4f6466103a8ba7870a1356e2b4d1
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-20 07:38:57 +0300

    feat(diaryProfiles): add endpoints to WeightLogController

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/diaryProfile/WeightLogController.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/diaryProfile/WeightLogController.java
@@ -1,11 +1,79 @@
 package com.github.plantfern.foodDiary.diaryProfiles.web.controllers.diaryProfile;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.plantfern.foodDiary.diaryProfiles.api.dto.WeightLogDto;
+import com.github.plantfern.foodDiary.diaryProfiles.domain.services.WeightLogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/diary-profile/weight-log")
 public class WeightLogController {
+
+
+    private final WeightLogService weightLogService;
+
+    public WeightLogController(WeightLogService weightLogService) {
+        this.weightLogService = weightLogService;
+    }
+
+    @PostMapping("/{diaryProfileId}")
+    public ResponseEntity<WeightLogDto> create(
+            @PathVariable Long diaryProfileId,
+            @RequestParam Float weight
+    ) {
+
+        return ResponseEntity.ok(weightLogService
+                .create(
+                        diaryProfileId,
+                        weight
+                )
+        );
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<WeightLogDto> update(
+            @PathVariable Long commentId,
+            @RequestParam Float weight
+    ) {
+
+        return ResponseEntity.ok(weightLogService
+                .update(
+                        commentId,
+                        weight
+                )
+        );
+    }
+
+    @DeleteMapping("/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable Long commentId
+    ) {
+
+        weightLogService.delete(commentId);
+    }
+
+    @GetMapping("/get-all/{diaryProfileId}")
+    public ResponseEntity<List<WeightLogDto>> getByDiaryProfile(
+            @PathVariable Long diaryProfileId
+    ) {
+
+        return ResponseEntity.ok(weightLogService
+                .getByDiaryProfileId(diaryProfileId));
+    }
+
+    @GetMapping("/get-latest/{diaryProfileId}")
+    public ResponseEntity<WeightLogDto> getLatestByDiaryProfile(
+            @PathVariable Long diaryProfileId
+    ) {
+
+        return ResponseEntity.ok(weightLogService
+                .getLatestByDiaryProfileId(diaryProfileId)
+        );
+    }
 }
