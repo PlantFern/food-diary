06f7c9808e93688381597c473e19f6167e0f855c
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-21 00:14:18 +0300

    feat(food): recipe methods for repositories

--- src/main/java/com/github/plantfern/foodDiary/food/api/dto/PersonalizedRecipeListItemDto.java
+++ src/main/java/com/github/plantfern/foodDiary/food/api/dto/PersonalizedRecipeListItemDto.java
@@ -2,14 +2,11 @@ package com.github.plantfern.foodDiary.food.api.dto;
 
 public record PersonalizedRecipeListItemDto(
         Long recipeId,
-        String code,
         String name,
         String description,
-        String recipe,
         String photoPath,
         Float totalWeightGrams,
         boolean isPublic,
-        Long createdById,
         Boolean isFavorite
 ) {
 }
