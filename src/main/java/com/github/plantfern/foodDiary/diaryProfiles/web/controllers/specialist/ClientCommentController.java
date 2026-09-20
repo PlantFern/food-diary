180f2f08f8810d439075695d9acd1f5137e56c5c
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-20 20:20:48 +0300

    refactor(diaryProfile): add ClientCommentController

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/specialist/ClientCommentController.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/web/controllers/specialist/ClientCommentController.java
@@ -1,11 +1,95 @@
 package com.github.plantfern.foodDiary.diaryProfiles.web.controllers.specialist;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.plantfern.foodDiary.diaryProfiles.api.CommentableType;
+import com.github.plantfern.foodDiary.diaryProfiles.api.dto.DiaryCommentDto;
+import com.github.plantfern.foodDiary.diaryProfiles.domain.services.DiaryCommentService;
+import com.github.plantfern.foodDiary.diaryProfiles.web.requests.CommentRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 
 @RestController
 @RequestMapping("/api/specialist/comment")
 public class ClientCommentController {
+
+    private final DiaryCommentService diaryCommentService;
+
+    public ClientCommentController(DiaryCommentService diaryCommentService) {
+        this.diaryCommentService = diaryCommentService;
+    }
+
+    @PostMapping("/{diaryProfile}")
+    public ResponseEntity<DiaryCommentDto> create(
+            @PathVariable Long diaryProfile,
+            @ModelAttribute CommentRequest request
+    ) {
+
+        return ResponseEntity.ok(diaryCommentService.create(
+                        diaryProfile,
+                        request.commentableType(),
+                        request.commentableId(),
+                        request.body()
+                )
+        );
+    }
+
+    @PutMapping("/comments/{commentId}")
+    public ResponseEntity<DiaryCommentDto> update(
+            @PathVariable Long commentId,
+            @ModelAttribute CommentRequest request
+    ) {
+
+        return ResponseEntity.ok(diaryCommentService.update(
+                        commentId,
+                        request.commentableType(),
+                        request.commentableId(),
+                        request.body()
+                )
+        );
+    }
+
+    @GetMapping("/comments/{commentId}")
+    public ResponseEntity<DiaryCommentDto> getById(
+            @PathVariable Long commentId
+    ) {
+
+        return ResponseEntity.ok(diaryCommentService.getById(commentId));
+    }
+
+    @GetMapping("/diary-profile/{diaryProfileId}/get-by-comment-type")
+    public ResponseEntity<List<DiaryCommentDto>>  getAllByCommentType(
+            @PathVariable Long diaryProfileId,
+            @RequestParam String commentType
+    ) {
+
+        return ResponseEntity.ok(diaryCommentService
+                .getByDiaryProfileAndCommentableType(
+                        diaryProfileId,
+                        CommentableType.valueOf(commentType)
+                ));
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable Long commentId
+    ) {
+
+        diaryCommentService.delete(commentId);
+    }
+
+    @GetMapping("/get-by-ids")
+    public ResponseEntity<List<DiaryCommentDto>> getAllByIds(
+            @RequestParam Set<Long> commentIds
+    ) {
+
+        return ResponseEntity.ok(
+                diaryCommentService.getByCommentableIds(commentIds)
+        );
+    }
 }
