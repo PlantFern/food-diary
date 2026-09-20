c8ed4fe4b9b14be24249be391b32848c6eb054c9
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-20 22:40:48 +0300

    feat(diaryProfile): add product moderation endpoints

---
+++ src/main/java/com/github/plantfern/foodDiary/food/web/controllers/moderation/ProductModerationController.java
@@ -0,0 +1,40 @@
+package com.github.plantfern.foodDiary.food.web.controllers.moderation;
+
+import com.github.plantfern.foodDiary.food.api.dto.ProductDetailDto;
+import com.github.plantfern.foodDiary.food.api.dto.ProductListItemDto;
+import com.github.plantfern.foodDiary.food.api.dto.ProductSearchRequest;
+import com.github.plantfern.foodDiary.food.domain.services.ProductQueryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/moderation/products")
+public class ProductModerationController {
+
+    private final ProductQueryService productQueryService;
+
+    public ProductModerationController(ProductQueryService productQueryService) {
+        this.productQueryService = productQueryService;
+    }
+
+    @GetMapping
+    public Page<ProductListItemDto> getAll(
+            @RequestParam(required = false) String query,
+            @RequestParam(required = false) Long categoryId,
+            Pageable pageable
+    ) {
+        return productQueryService.getAll(
+                new ProductSearchRequest(query, categoryId),
+                pageable
+        );
+    }
+
+    @GetMapping("/{productId}")
+    public ProductDetailDto getDetail(
+            @PathVariable Long productId
+    ) {
+        return productQueryService.getDetailAboutProduct(productId);
+    }
+}
