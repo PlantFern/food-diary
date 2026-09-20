afa98e032428ac2ce61caaf7f927f111907a3d99
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-20 15:53:06 +0300

    refactor(food): remove redundant goal ID field
    Goal nutrients always go along with the goal so their own ID is unnecessary

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/api/dto/GoalNutrientDto.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/api/dto/GoalNutrientDto.java
@@ -3,7 +3,6 @@ package com.github.plantfern.foodDiary.diaryProfiles.api.dto;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.GoalEntity;
 
 public record GoalNutrientDto(
-        Long goalId,
         Long nutrientId,
         Float amount
 ) {
