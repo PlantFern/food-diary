fb81a936e6b76d648f78b5a02e8f8f85a0cf3095
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-20 16:24:40 +0300

    refactor(food): configure goal and nutrient goal mappers
    - Add nutrient set to goal dto
    - Add mapping configuration for toDto and toEntity in GoalMapper
    - Add mapping configuration for toEntity in GoalNutrientMapper

--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/mappers/GoalMapper.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/mappers/GoalMapper.java
@@ -1,20 +1,34 @@
 package com.github.plantfern.foodDiary.diaryProfiles.domain.mappers;
 
 
-import com.github.plantfern.foodDiary.diaryProfiles.api.dto.DiaryCommentDto;
 import com.github.plantfern.foodDiary.diaryProfiles.api.dto.GoalDto;
-import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.DiaryCommentEntity;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.GoalEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        uses = GoalNutrientMapper.class
+)
 public interface GoalMapper {
 
+    @Mapping(
+            target = "nutrientSet",
+            source = "goalNutrientSet"
+    )
     GoalDto toDto(GoalEntity goalEntity);
 
+    @Mapping(target = "goalNutrientSet", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "createdById", ignore = true)
+    @Mapping(target = "plannedEndDate", ignore = true)
+    @Mapping(target = "actualEndDate", ignore = true)
+    @Mapping(target = "startDate", ignore = true)
+    @Mapping(target = "plannedWeight", ignore = true)
+    @Mapping(target = "diaryProfileId", ignore = true)
     GoalEntity toEntity(GoalDto goalDto);
 
     List<GoalDto> toDtoList(List<GoalEntity> goalEntityList);
--- src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/mappers/GoalNutrientMapper.java
+++ src/main/java/com/github/plantfern/foodDiary/diaryProfiles/domain/mappers/GoalNutrientMapper.java
@@ -1,11 +1,10 @@
 package com.github.plantfern.foodDiary.diaryProfiles.domain.mappers;
 
 
-import com.github.plantfern.foodDiary.diaryProfiles.api.dto.GoalDto;
 import com.github.plantfern.foodDiary.diaryProfiles.api.dto.GoalNutrientDto;
-import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.GoalEntity;
 import com.github.plantfern.foodDiary.diaryProfiles.domain.entities.GoalNutrientEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -15,6 +14,11 @@ public interface GoalNutrientMapper {
 
     GoalNutrientDto toDto(GoalNutrientEntity goalNutrientEntity);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "goal", ignore = true)
+    @Mapping(target = "goalId", ignore = true)
+    @Mapping(target = "nutrientId", ignore = true)
+    @Mapping(target = "amount", ignore = true)
     GoalNutrientEntity toEntity(GoalNutrientDto goalNutrientDto);
 
     List<GoalNutrientDto> toDtoList(List<GoalNutrientEntity> goalNutrientEntityList);
