3c5190b7e9d5c7dd3fc7b859a866a7c33c346376
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-21 00:15:12 +0300

    feat(food): remove redundant arguments

--- src/main/java/com/github/plantfern/foodDiary/food/api/dto/RecipeListItemDto.java
+++ src/main/java/com/github/plantfern/foodDiary/food/api/dto/RecipeListItemDto.java
@@ -2,11 +2,7 @@ package com.github.plantfern.foodDiary.food.api.dto;
 
 public record RecipeListItemDto(
         Long recipeId,
-        String recipeCode,
-        String recipeName,
-        String photoPath,
-        Float totalWeightGrams,
-        Float kcalPer100g,
-        Boolean isFavorite
+        String name,
+        String photoPath
 ) {
 }
