504a26a40c4cf43342e882dc0925648ec8e02f94
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 02:09:11 +0300

    refactor(food): add dto for personalized recipe details

---
+++ src/main/java/com/github/plantfern/foodDiary/food/api/dto/PersonalizedRecipeDetailDto.java
@@ -0,0 +1,17 @@
+package com.github.plantfern.foodDiary.food.api.dto;
+
+import java.util.List;
+
+public record PersonalizedRecipeDetailDto(
+        Long recipeId,
+        String recipeName,
+        String recipeDescription,
+        String recipeText,
+        String photoPath,
+        Float totalWeightGrams,
+        String entityStatusCode,
+        Boolean isPublic,
+        Boolean isFavorite,
+    List<RecipeComponentDto> components
+) {
+}
