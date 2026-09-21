2cdf2df360a461050902b4707aac6b205e6a7711
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-21 08:59:49 +0300

    feat(food): Add create method for BrandedProductService and add @Service

--- src/main/java/com/github/plantfern/foodDiary/food/domain/services/BrandedProductService.java
+++ src/main/java/com/github/plantfern/foodDiary/food/domain/services/BrandedProductService.java
@@ -1,4 +1,50 @@
 package com.github.plantfern.foodDiary.food.domain.services;
 
+import com.github.plantfern.foodDiary.food.api.dto.BrandedProductDto;
+import com.github.plantfern.foodDiary.food.domain.entities.BrandedProductEntity;
+import com.github.plantfern.foodDiary.food.domain.mappers.BrandedProductMapper;
+import com.github.plantfern.foodDiary.food.domain.repositories.BrandedProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BrandedProductService {
+
+    private final ProductService productService;
+    private final FoodServingService foodServingService;
+    private final BrandedProductMapper brandedProductMapper;
+    private final BrandedProductRepository brandedProductRepository;
+
+    public BrandedProductService(ProductService productService, FoodServingService foodServingService, BrandedProductMapper brandedProductMapper, BrandedProductRepository brandedProductRepository) {
+        this.productService = productService;
+        this.foodServingService = foodServingService;
+        this.brandedProductMapper = brandedProductMapper;
+        this.brandedProductRepository = brandedProductRepository;
+    }
+
+    public BrandedProductDto create(
+            Long productId,
+            String barcode,
+            Long baseServingId
+    ) {
+
+        if(barcode.isEmpty())
+            throw new IllegalArgumentException("Barcode cannot be empty");
+
+        if(!productService.existsById(productId))
+            throw new EntityNotFoundException("Product not found");
+
+        if(!foodServingService.existsById(baseServingId))
+            throw new EntityNotFoundException("Product serving not found");
+
+        return brandedProductMapper.toDto(
+                brandedProductRepository.save(
+                        new BrandedProductEntity(
+                                productId,
+                                barcode,
+                                baseServingId
+                        )
+                )
+        );
+    }
 }
