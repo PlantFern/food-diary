88846c3bde2f06f2f146a5ae3c4919c404abe926
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-21 09:00:26 +0300

    fix(food): Remove redundant productId field

--- src/main/java/com/github/plantfern/foodDiary/food/api/dto/FoodServingDto.java
+++ src/main/java/com/github/plantfern/foodDiary/food/api/dto/FoodServingDto.java
@@ -2,7 +2,6 @@ package com.github.plantfern.foodDiary.food.api.dto;
 
 public record FoodServingDto(
         Long id,
-        Long productId,
         String itemType,
         String itemId,
         String amount,
