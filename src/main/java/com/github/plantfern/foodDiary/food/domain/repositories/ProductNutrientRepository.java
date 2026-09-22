0a95e6815aa0059f26b99db13f0e613737a04048
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 21:21:31 +0300

    feat(food): add method to find product nutrient by product ID and nutrient ID

--- src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/ProductNutrientRepository.java
+++ src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/ProductNutrientRepository.java
@@ -5,7 +5,15 @@ import com.github.plantfern.foodDiary.food.domain.entities.ProductNutrientEntity
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 
 @Repository
 public interface ProductNutrientRepository extends JpaRepository<ProductNutrientEntity, Long> {
+
+    List<ProductNutrientEntity> findAllByProductIdInAndNutrientId(
+            Collection<Long> productId,
+            Long nutrientId
+    );
 }
