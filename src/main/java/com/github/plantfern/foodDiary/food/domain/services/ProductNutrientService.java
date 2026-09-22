b66c672194c6fe83de7cb493506bbdeb5000f2bb
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 15:49:43 +0300

    refactor(meals): add and implement method to add multiple nutrients for one product

--- src/main/java/com/github/plantfern/foodDiary/food/domain/services/ProductNutrientService.java
+++ src/main/java/com/github/plantfern/foodDiary/food/domain/services/ProductNutrientService.java
@@ -5,11 +5,14 @@ import com.github.plantfern.foodDiary.common.services.NutrientService;
 import com.github.plantfern.foodDiary.food.api.EntityStatus;
 import com.github.plantfern.foodDiary.food.domain.entities.ProductNutrientEntity;
 import com.github.plantfern.foodDiary.food.domain.repositories.ProductNutrientRepository;
+import com.github.plantfern.foodDiary.food.domain.repositories.ProductRepository;
 import com.github.plantfern.foodDiary.food.domain.security.FoodPolicy;
 import com.github.plantfern.foodDiary.users.api.CurrentUser;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -22,13 +25,15 @@ public class ProductNutrientService {
     private final FoodPolicy foodPolicy;
     private final CurrentUser currentUser;
     private final ProductNutrientRepository productNutrientRepository;
+    private final ProductRepository productRepository;
 
-    public ProductNutrientService(ProductService productService, NutrientService nutrientService, FoodPolicy foodPolicy, CurrentUser currentUser, ProductNutrientRepository productNutrientRepository) {
+    public ProductNutrientService(ProductService productService, NutrientService nutrientService, FoodPolicy foodPolicy, CurrentUser currentUser, ProductNutrientRepository productNutrientRepository, ProductRepository productRepository) {
         this.productService = productService;
         this.nutrientService = nutrientService;
         this.foodPolicy = foodPolicy;
         this.currentUser = currentUser;
         this.productNutrientRepository = productNutrientRepository;
+        this.productRepository = productRepository;
     }
 
     public Long addToProduct(Long productId, Long nutrientId, Float amount){
@@ -52,6 +57,48 @@ public class ProductNutrientService {
                         nutrient.getId()
                 )
         ).getId();
+    }
+
+    public List<Long> addAllToProduct(
+            Long productId,
+            Map<Long, Float> nutrients
+    ) {
+
+        var foundProductId = productRepository.findById(productId)
+                .orElseThrow(
+                        () -> new EntityNotFoundException("Product not found")
+                )
+                .getId();
+
+        var nutrientIds = nutrients.keySet();
+
+        nutrientService.existsAllByIdIn(
+                nutrientIds
+        );
+
+        var productNutrientList = new ArrayList<ProductNutrientEntity>();
+
+        nutrients.forEach(
+                (nutrientId, amount) -> {
+                    if(amount == null || amount <= 0)
+                        throw new IllegalArgumentException("Amount must be greater than 0");
+
+                    productNutrientList.add(
+                            new ProductNutrientEntity(
+                                    amount,
+                                    nutrientId,
+                                    foundProductId
+                            )
+                    );
+                }
+        );
+
+        return productNutrientRepository.saveAll(
+                productNutrientList
+        )
+                .stream()
+                .map(ProductNutrientEntity::getId)
+                .toList();
     }
 
     public Long updateProductNutrient(Long productNutrientId, Float amount){
