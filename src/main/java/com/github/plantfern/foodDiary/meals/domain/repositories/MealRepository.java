e822709b1458ee4cf93cffc73ebc0ddf50b3a35f
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 13:11:19 +0300

    refactor(meals): change find method to use meal date
    Now meals will find by meal date, not creation date

--- src/main/java/com/github/plantfern/foodDiary/meals/domain/repositories/MealRepository.java
+++ src/main/java/com/github/plantfern/foodDiary/meals/domain/repositories/MealRepository.java
@@ -2,16 +2,22 @@ package com.github.plantfern.foodDiary.meals.domain.repositories;
 
 
 import com.github.plantfern.foodDiary.meals.domain.entities.MealEntity;
+import com.github.plantfern.foodDiary.meals.domain.entities.MealTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MealRepository extends JpaRepository<MealEntity, Long> {
 
-    List<MealEntity> findAllByDiaryProfileIdAndCreatedAtBetween(
+    List<MealEntity> findAllByDiaryProfileIdAndDate(
+            Long diaryProfileId, LocalDate date
+    );
+
             Long diaryProfileId,
             LocalDateTime createdAt,
             LocalDateTime createdAt2
