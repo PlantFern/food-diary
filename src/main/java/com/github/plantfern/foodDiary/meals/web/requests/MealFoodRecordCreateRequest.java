eb6a3eacee0ee028712de58b276009c62dee08fe
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 14:28:54 +0300

    feat(meals): add necessary parameters to request record

--- src/main/java/com/github/plantfern/foodDiary/meals/web/requests/MealFoodRecordCreateRequest.java
+++ src/main/java/com/github/plantfern/foodDiary/meals/web/requests/MealFoodRecordCreateRequest.java
@@ -1,11 +1,15 @@
 package com.github.plantfern.foodDiary.meals.web.requests;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 
 public record MealFoodRecordCreateRequest(
+        Long mealId,
+        Long mealTypeId,
         Long servingId,
         Float amount,
-        LocalDate date
+        LocalDate date,
+        LocalTime eatenAt
 ) {
 }
