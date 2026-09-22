8c82de1ea257be35a6184eae8e1e99c529cd67d6
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 14:46:11 +0300

    refactor(meals): add meal and record data to request

--- src/main/java/com/github/plantfern/foodDiary/meals/web/requests/QuickProductCreateRequest.java
+++ src/main/java/com/github/plantfern/foodDiary/meals/web/requests/QuickProductCreateRequest.java
@@ -1,12 +1,19 @@
 package com.github.plantfern.foodDiary.meals.web.requests;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Map;
 import java.util.Set;
 
 public record QuickProductCreateRequest(
+        Long mealId,
+        Long mealTypeId,
+        LocalDate date,
+        LocalTime eatenAt,
+        Float amount,
         String name,
         String description,
-        Float weight,
+        Float gramWeight,
         Map<Long, Float> nutrients
 ) {
 }
