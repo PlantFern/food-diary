cb3322b620e58bcd04a437285bb9c71a02f66341
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-22 14:46:43 +0300

    refactor(meals): add date parameter to request

--- src/main/java/com/github/plantfern/foodDiary/meals/web/requests/MealCreateRequest.java
+++ src/main/java/com/github/plantfern/foodDiary/meals/web/requests/MealCreateRequest.java
@@ -2,10 +2,12 @@ package com.github.plantfern.foodDiary.meals.web.requests;
 
 import jakarta.annotation.Nullable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record MealCreateRequest(
         Long mealTypeId,
+        LocalDate date,
         @Nullable Long templateId,
         @Nullable String photoPath,
         @Nullable LocalDateTime eatenAt
