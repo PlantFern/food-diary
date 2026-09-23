d4d2a1e1f7e3d12dacf07f3050f98e20c8c71e76
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-23 15:20:04 +0300

    fix(food): fix dto parameter types

--- src/main/java/com/github/plantfern/foodDiary/food/api/dto/FoodServingDto.java
+++ src/main/java/com/github/plantfern/foodDiary/food/api/dto/FoodServingDto.java
@@ -3,9 +3,9 @@ package com.github.plantfern.foodDiary.food.api.dto;
 public record FoodServingDto(
         Long id,
         String itemType,
-        String itemId,
-        String amount,
-        String gramWeight,
+        Long itemId,
+        Float amount,
+        Float gramWeight,
         ServingUnitDto servingUnit,
         String description
 ) { }
