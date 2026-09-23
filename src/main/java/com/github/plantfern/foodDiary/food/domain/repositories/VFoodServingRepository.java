b2ace540041d0824c3b2d093af79cdfbbf7a7298
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-23 15:29:30 +0300

    fix(food): add @Repository annotation for repositories

--- src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/VFoodServingRepository.java
+++ src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/VFoodServingRepository.java
@@ -1,13 +1,17 @@
 package com.github.plantfern.foodDiary.food.domain.repositories;
 
+
 import com.github.plantfern.foodDiary.food.domain.views.VFoodServingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
 
+
+@Repository
 public interface VFoodServingRepository extends JpaRepository<VFoodServingEntity, Long> {
 
     List<VFoodServingEntity> findAllByServingIdIn(
--- src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/VItemNutrientRepository.java
+++ src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/VItemNutrientRepository.java
@@ -1,11 +1,15 @@
 package com.github.plantfern.foodDiary.food.domain.repositories;
 
+
 import com.github.plantfern.foodDiary.food.domain.views.VItemNutrientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
 
+
+@Repository
 public interface VItemNutrientRepository extends JpaRepository<VItemNutrientEntity, Long> {
 
     List<VItemNutrientEntity> findAllByItemTypeAndItemIdAndNutrientId(
