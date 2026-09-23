1e1cf795fd4f44033e033f0d626046b0e4fa158e
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-23 14:26:44 +0300

    refactor(food): add repositories for view entities

---
+++ src/main/java/com/github/plantfern/foodDiary/food/domain/repositories/VItemNutrientRepository.java
@@ -0,0 +1,16 @@
+package com.github.plantfern.foodDiary.food.domain.repositories;
+
+import com.github.plantfern.foodDiary.food.domain.views.VItemNutrientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface VItemNutrientRepository extends JpaRepository<VItemNutrientEntity, Long> {
+
+    List<VItemNutrientEntity> findAllByItemTypeAndItemIdAndNutrientIdIn(
+            String itemType,
+            Long itemId,
+            Collection<Long> nutrientId
+    );
+}
