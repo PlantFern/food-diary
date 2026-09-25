daceaf3228ff7338b33246cb529d014c8eb932ce
Staykova_Alona <380666091645Abyss@gmail.com>
2026-09-25 19:56:39 +0300

    feat(users/web): make register endpoint return UserDto
    - Add nullable login parameter
    - Call UserService.register()
    - Add UserService dependency
    
    Return  created user data to the client to avoid an extra request.

--- src/main/java/com/github/plantfern/foodDiary/users/web/controllers/AuthController.java
+++ src/main/java/com/github/plantfern/foodDiary/users/web/controllers/AuthController.java
@@ -1,17 +1,34 @@
 package com.github.plantfern.foodDiary.users.web.controllers;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.plantfern.foodDiary.users.api.UserDto;
+import com.github.plantfern.foodDiary.users.domain.UserService;
+import jakarta.validation.constraints.Null;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class AuthController {
 
-    @PostMapping("/registration")
-    public void createUserAccount(@ModelAttribute String email,
-                                  @ModelAttribute String password){
+    private final UserService userService;
 
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registration")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<UserDto> createUserAccount(@ModelAttribute String email,
+                                                     @ModelAttribute String password,
+                                                     @ModelAttribute @Null String login
+    ){
+
+        return ResponseEntity.ok(
+                userService.register(
+                        email,
+                        password,
+                        login)
+        );
     }
 }
